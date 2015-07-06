17e9367dd265d73d993bb333d96098a149884c0c
Jay Butera <buterajay@gmail.com>
2015-07-06 17:10:43 -0500

    Initial serial comm class implementation

--- src/Communication/serialCom.cpp
+++ src/Communication/serialCom.cpp
@@ -1,5 +1,45 @@
-#include <termios.h>
+//#include <termios.h>
+#include <boost/asio.hpp>
+#include <string>
 
-int main() {
-    return 0;
-}
+class Serial
+{
+public:
+    Serial(std::string port,
+           unsigned int baud_rate=115200):
+        io(), serial(io, port)
+    {
+        serial.set_option(boost::asio::serial_port_base::baud_rate(baud_rate));
+    }
+
+    void writeString(std::string s)
+    {
+        boost::asio::write(serial,boost::asio::buffer(s.c_str(),s.size()));
+    }
+
+    std::string readLine()
+    {
+        using namespace boost;
+        char c;
+        std::string result;
+
+        while(true)
+        {
+            asio::read(serial, asio::buffer(&c, 1));
+            switch(c)
+            {
+                case '\r':
+                    break;
+                case '\n':
+                    return result;
+                default:
+                    result+=c;
+            }
+        }
+    }
+
+private:
+    boost::asio::io_service io;
+    boost::asio::serial_port serial;
+};
+//SimpleSerial serial("/dev/tty.usbxx",115200); // get SimpleSerial class on link above
